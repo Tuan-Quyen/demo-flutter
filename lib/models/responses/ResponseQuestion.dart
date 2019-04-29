f542bd84b6029a6e3ea065cccfac3412333dfc72
ctuanquyen97 <ctuanquyen97@gmail.com>
2019-04-29 17:26:30 +0700

    +Design list tags

--- lib/models/responses/ResponseQuestion.dart
+++ lib/models/responses/ResponseQuestion.dart
@@ -4,13 +4,27 @@ import 'package:json_annotation/json_annotation.dart';
 class ResponseQuestion {
   String topicName;
   bool hasAnswer;
+  int topicScore;
+  int topicAnswer;
+  List<String> topicTag;
+  int questionId;
 
-  ResponseQuestion({this.topicName, this.hasAnswer});
+  ResponseQuestion(
+      {this.topicName,
+      this.hasAnswer,
+      this.topicScore,
+      this.topicAnswer,
+      this.questionId,
+      this.topicTag});
 
   factory ResponseQuestion.fromJson(Map<String, dynamic> json) {
     return ResponseQuestion(
-        topicName: json["title"],
-        hasAnswer: json["is_answered"]
+        topicName: json['title'],
+        hasAnswer: json['is_answered'],
+        topicScore: json['score'],
+        topicAnswer: json['answer_count'],
+        questionId: json['question_id'],
+        topicTag: json['tags']
     );
   }
 }
