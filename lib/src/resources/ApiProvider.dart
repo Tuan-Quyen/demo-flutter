b6c3ee92eebd870f6fe2a9f5b1e7e683b5103d66
ctuanquyen97 <ctuanquyen97@gmail.com>
2019-05-27 16:17:12 +0700

    +fix baseUrl

--- lib/src/resources/ApiProvider.dart
+++ lib/src/resources/ApiProvider.dart
@@ -6,11 +6,12 @@ import '../models/ItemModels.dart';
 class ApiProvider {
   Client client = Client();
   final _apiKey = '4d560f483fdb51909f54bc37635d7a2c';
+  final _baseUrl = 'http://api.themoviedb.org/3/movie';
 
   Future<ItemModel> fetchMovieList(int _page) async {
     print("entered");
     final response = await client
-        .get("http://api.themoviedb.org/3/movie/popular?api_key=$_apiKey&page=$_page");
+        .get("$_baseUrl/popular?api_key=$_apiKey&page=$_page");
     if (response.statusCode == 200) {
       print("reponse : " + response.body.toString());
       return ItemModel.fromJson(json.decode(response.body));
