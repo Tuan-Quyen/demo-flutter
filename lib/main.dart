9195193540e5935e06f45a76f203b7d0669d2505
quyen.chau <quyen.chau@apps-cyclone.com>
2019-05-29 14:43:31 +0700

    +naviga route home and movie

--- lib/main.dart
+++ lib/main.dart
@@ -1,9 +1,15 @@
+import 'package:bloc/bloc.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter/services.dart';
+import 'package:flutter_app/src/blocs/SimpleDelegateBloc.dart';
+import 'package:flutter_app/src/ui/HomePage.dart';
 import 'package:flutter_app/src/ui/MoviePage.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
+
 void main() {
   SystemChrome.setPreferredOrientations(
       [DeviceOrientation.portraitUp, DeviceOrientation.portraitDown]).then((_) {
+    BlocSupervisor.delegate = SimpleBlocDelegate();
     runApp(MyApp());
   });
 }
@@ -12,27 +18,17 @@ class MyApp extends StatelessWidget {
   // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
+    CounterBloc counterBloc = CounterBloc();
     return MaterialApp(
         title: 'Movie Page',
         theme: ThemeData(
           primarySwatch: Colors.blue,
         ),
-        home: MoviePage());
+        home: BlocProvider(bloc: counterBloc, child: HomePage()),
+        routes: <String, WidgetBuilder>{
+          '/HomePage': (BuildContext context) =>
+              BlocProvider(bloc: counterBloc, child: HomePage()),
+          '/MoviePage': (BuildContext context) => MoviePage(),
+        });
   }
 }
-/*class MyApp extends StatelessWidget {
-  final CounterBloc _counterBloc = CounterBloc();
-  // This widget is the root of your application.
-  @override
-  Widget build(BuildContext context) {
-    return MaterialApp(
-        title: 'Flutter Demo',
-        theme: ThemeData(
-          primarySwatch: Colors.blue,
-        ),
-        home: BlocProvider<CounterBloc>(
-          bloc: _counterBloc,
-          child: HomePage(),
-        ),);
-  }
-}*/
--- lib/src/ui/HomePage.dart
+++ lib/src/ui/HomePage.dart
@@ -4,12 +4,34 @@ import 'package:bloc/bloc.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
 
-class HomePage extends StatelessWidget {
+class HomePage extends StatefulWidget {
+  @override
+  _HomePageState createState() => _HomePageState();
+}
+
+class _HomePageState extends State<HomePage> {
+  CounterBloc _counterBloc;
   @override
   Widget build(BuildContext context) {
-    final CounterBloc _counterBloc = BlocProvider.of<CounterBloc>(context);
     return Scaffold(
-      appBar: AppBar(title: Text('Counter')),
+      appBar: AppBar(
+        title: Text('Counter'),
+        actions: <Widget>[
+          Container(
+            margin: const EdgeInsets.only(right: 10),
+            child: IconButton(
+              onPressed: () {
+                Navigator.of(context).pushNamed("/MoviePage");
+              },
+              icon: Icon(
+                Icons.navigate_next,
+                size: 20,
+              ),
+              color: Colors.white,
+            ),
+          )
+        ],
+      ),
       body: BlocBuilder<CounterEvent, int>(
         bloc: _counterBloc,
         builder: (BuildContext context, int count) {
@@ -28,6 +50,7 @@ class HomePage extends StatelessWidget {
           Padding(
             padding: EdgeInsets.symmetric(vertical: 5.0),
             child: FloatingActionButton(
+              heroTag: 0,
               child: Icon(Icons.add),
               onPressed: () {
                 _counterBloc.dispatch(CounterEvent.increment);
@@ -37,10 +60,10 @@ class HomePage extends StatelessWidget {
           Padding(
             padding: EdgeInsets.symmetric(vertical: 5.0),
             child: FloatingActionButton(
+              heroTag: 1,
               child: Icon(Icons.remove),
               onPressed: () {
                 _counterBloc.dispatch(CounterEvent.decrement);
-
               },
             ),
           ),
@@ -48,13 +71,24 @@ class HomePage extends StatelessWidget {
       ),
     );
   }
+
+
+  @override
+  void initState() {
+    super.initState();
+    _counterBloc = BlocProvider.of<CounterBloc>(context);
+  }
+
+  @override
+  void dispose() {
+    _counterBloc.dispose();
+    super.dispose();
+  }
 }
 
 enum CounterEvent { increment, decrement }
 
 class CounterBloc extends Bloc<CounterEvent, int> {
-
-
   @override
   Stream<int> mapEventToState(CounterEvent event) async* {
     switch (event) {
